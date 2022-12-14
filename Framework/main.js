e839aa24dc37604fc9f38a099c0f2897642a118e
Thanabodink <kaijaepop@gmail.com>
2022-12-14 23:43:13 +0700

    404 Error

--- Framework/main.js
+++ Framework/main.js
@@ -1,1 +1,10 @@
-console.log("mamama")
+const express = require('express')
+const app = express()
+
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.listen(3000, () => {
+    console.log("Start Server at Port [3000]")
+})
