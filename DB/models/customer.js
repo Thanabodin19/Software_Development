a53ebdbd2a6b80df8fe1467cb983ea5129af4dc2
Thanabodink <kaijaepop@gmail.com>
2022-12-28 23:00:40 +0700

    Database

---
+++ DB/models/customer.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const CustomerSchema = mongoose.Schema(
+    {
+        CustomerId: Number,
+        Fullname:   String,
+        Address:    String
+    },
+    {
+        versionKey: false
+    }
+)
+
+module.exports = mongoose.model('Customer',CustomerSchema)
